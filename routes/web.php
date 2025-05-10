52093cec7e5fd23192e0977f9b0de1d0df52df24
Adith Widhiantara <adithwidhiantara@gmail.com>
2025-05-10 10:43:43 +0700

    Feat: Auth

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,27 @@
 <?php
 
-use App\Enums\UserType;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\RoleMiddleware;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\BookCreatorController;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::middleware(['auth', RoleMiddleware::class . ':' . UserType::ADMIN])->group(function () {
-    Route::resource('users', UserController::class);
-    Route::resource('book-creators', BookCreatorController::class);
-    Route::resource('books', BookController::class)->except(['index', 'show']);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('books', [BookController::class, 'index'])->name('books.index');
-    Route::get('books/{book}', [BookController::class, 'show'])->name('books.show');
-});
+require __DIR__.'/auth.php';
