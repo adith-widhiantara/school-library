0320afe025aa7a00d3b0f687c285cb9b569abee2
Adith Widhiantara <adithwidhiantara@gmail.com>
2025-05-10 11:12:37 +0700

    Update: add web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Enums\UserType;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BookCreatorController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +27,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', RoleMiddleware::class.':'.UserType::ADMIN])->group(function () {
+    Route::resource('users', UserController::class);
+    Route::resource('book-creators', BookCreatorController::class);
+    Route::resource('books', BookController::class)->except(['index', 'show']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('books', [BookController::class, 'index'])->name('books.index');
+    Route::get('books/{book}', [BookController::class, 'show'])->name('books.show');
 });
 
 require __DIR__.'/auth.php';
